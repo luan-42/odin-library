c2b62a54110450be3ce4125e011a5d60ea85c993
Luan Ferreira <178341285+luan-42@users.noreply.github.com>
2024-11-20 11:53:39 -0300

    :sparkles: Adicionar opção new-book

--- app.js
+++ app.js
@@ -10,14 +10,18 @@ function Book(cover, title, author, pages, description, read) {
 }
 
 function addBookToLibrary(cover, title, author, pages, description, read) {
+    title = title == "" ? "Untitled": title;
     cover = cover == "" ? "./images/no-image.jpg": cover;
     const book = new Book(cover, title, author, pages, description, read);
     myLibrary.push(book);
 }
 
+const newBook = document.querySelector("#new-book");
+
 function displayBooks(books) {
     const library = document.querySelector("#my-library");
     library.innerHTML = "";
+    library.appendChild(newBook);
     let index = 0;
     books.forEach((book) => {
         const cardBook = document.createElement("div");
@@ -36,3 +40,27 @@ function displayBooks(books) {
         index++;
     });
 }
+
+const modal = document.querySelector("#add-book-form");
+newBook.addEventListener("click", () => {
+    modal.showModal();
+});
+
+const addBook = document.querySelector("#btn-add-book");
+addBook.addEventListener("click", () => {
+    const book = [];
+    const inputs = modal.querySelectorAll("input, textarea");
+
+    inputs.forEach((input) => {
+        if (input.type != 'checkbox') {
+            book.push(input.value);
+            input.value = "";
+        } else {
+            book.push(input.checked);
+            input.checked = false;
+        }
+    })
+    addBookToLibrary(...book);
+    modal.close();
+    displayBooks(myLibrary);
+});
